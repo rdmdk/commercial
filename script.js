23282ecca280befa6f955f9da513d9210843aea4
Redmond <rdmdk@users.noreply.github.com>
2023-11-20 13:51:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function setup() {
+function setup_commercial() {
+  let jsn;
+  
   // Load previous config if available
-
+  if (localStorage.commercial_json) jsn = JSON.parse(localStorage.commercial_json);
+  
   // Setup new config
+  if (!jsn) {
+    //
+  }
+}
+
+function play_commercial() {
+  //
 }
 
 setup();
